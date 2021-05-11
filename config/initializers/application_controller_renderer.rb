39af1a1e5d1d37c0df3f16c23fff459202bed258
P1zza-L1cht <haruki21corder@gmail.com>
2021-05-11 05:46:45 +0000

    [finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+ Refile.secret_key = 'f4bb7493c1e54c4983a61efdceb390f5c2b6b998dd8538dc69eaaa209365647f15fca8b4134aa5362902a4ded890f65b73d7b957cdd031e905cef09cae770d9d'
